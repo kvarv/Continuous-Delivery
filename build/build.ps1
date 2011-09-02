b5bf9e4a68b6df37c159f8473854d4c9833d4845
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2011-09-02 15:55:35 +0200

    running tests with build

--- build/build.ps1
+++ build/build.ps1
@@ -3,13 +3,28 @@ $framework = '4.0'
 properties {
 	$base_dir = resolve-path .\..
 	$source_dir = "$base_dir\src"
+	$build_artifacts_dir = "$base_dir\build_artifacts"
+	$tools_dir = "$base_dir\tools"
 	$config = "Debug"
+	$test_dir = "$build_artifacts_dir\$config\Tests"
 }
 
 task default -depends local
  
-task local -depends compile
+task local -depends compile, test
 
-task compile {
-	exec { msbuild  $source_dir\ContinuousDelivery.sln /t:Clean /t:Build /p:Configuration=$config /v:n /nologo }
+task compile -depends clean {
+	exec { msbuild  $source_dir\ContinuousDelivery.sln /t:Clean /t:Build /p:Configuration=$config /v:q /nologo }
+}
+
+task clean {
+	rd $build_artifacts_dir -recurse -force  -ErrorAction SilentlyContinue | out-null
+	mkdir $build_artifacts_dir  -ErrorAction SilentlyContinue  | out-null
+}
+
+task test {	
+	$testassemblies = get-childitem $test_dir -recurse -include *tests*.dll
+	exec { 
+		& $tools_dir\NUnit-2.5.10\nunit-console-x86.exe $testassemblies /nologo /nodots /xml=$test_dir\tests_results.xml; 
+	}
 }
