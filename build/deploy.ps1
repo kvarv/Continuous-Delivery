cc7141c7c586b62bbe03321632aad681cee43df3
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2012-02-16 23:33:51 +0100

    added redeploy validation

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -5,7 +5,7 @@ include .\..\tools\Replace-Tokens\Replace-Tokens.ps1
 
 task default -depends deploy 
 
-task deploy -depends set_build_number, migrate_database {
+task deploy -depends set_build_number, validate_build_number, migrate_database {
     $app_path = "$build_artifacts_dir\ContinuousDelivery.WpfApplication"
     $config_file = "ContinuousDelivery.WpfApplication.exe.config"
     replace_tokens $app_config_data "$configs_dir\$config_file.template" "$app_path\$config_file"
@@ -17,4 +17,14 @@ task set_build_number {
 
 task migrate_database {
     exec { & $tools_dir\RoundhousE\rh.exe /s=$database_server /d=$database_name /f=$database_dir /trx /vf=$database_version_dll /silent} 
+}
+
+task validate_build_number -precondition { return $should_not_redeploy -eq 'True'} {
+    $webclient = New-Object System.Net.WebClient
+    $uri = New-Object System.Uri("http://localhost/guestAuth/app/rest/buildTypes/id:$env:build_configuration_id/builds/status:SUCCESS/number")
+    $last_successful_build_number = $webclient.DownloadString($uri)
+    if($build_number -eq $last_successful_build_number) {
+        Write-Output "##teamcity[buildStatus status='SUCCESS' text='Already deployed.']"
+        exit
+    }
 }
