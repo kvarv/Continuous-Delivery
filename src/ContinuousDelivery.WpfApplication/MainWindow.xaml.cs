dba1a2d3cb64f17cfdb87efe13a5fd245f090300
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2012-02-22 22:54:38 +0100

    added version information to MainWindow

--- src/ContinuousDelivery.WpfApplication/MainWindow.xaml.cs
+++ src/ContinuousDelivery.WpfApplication/MainWindow.xaml.cs
@@ -1,27 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Reflection;
+using Dapper;
 
-namespace WpfApplication
+namespace ContinuousDelivery.WpfApplication
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+			var connectionString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+			var connectionStringDetails = connectionString.Split(';');
+			var databaseServer = connectionStringDetails[0].Split('=')[1];
+			var database = connectionStringDetails[1].Split('=')[1];
+			var currentVersion = GetDatabaseVersion(connectionString);
+			connectionDetails.Text = string.Format("Connected to database version {0} of {1}@{2}", currentVersion, database, databaseServer);
+			assemblyVersion.Text = string.Format("Application version is {0}", Assembly.GetExecutingAssembly().GetName().Version);
+		}
+
+		private string GetDatabaseVersion(string connectionString)
+		{
+			string currentVersion;
+			using (var connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				var version = connection.Query("select top 1 * from RoundhousE.Version order by id desc").First();
+				currentVersion = version.version;
+			}
+			return currentVersion;
 		}
 	}
 }
