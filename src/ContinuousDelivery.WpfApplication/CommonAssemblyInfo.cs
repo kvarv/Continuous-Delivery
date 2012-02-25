f21653a34080006f62d34f19b7cb319fae9cfd87
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2012-02-25 23:25:24 +0100

    testing AssemblyInfo patcher in TC

--- src/ContinuousDelivery.WpfApplication/CommonAssemblyInfo.cs
+++ src/ContinuousDelivery.WpfApplication/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+//using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+//[assembly: AssemblyVersion("0.0.0.1")]
+//[assembly: AssemblyFileVersion("0.0.0.1")]
