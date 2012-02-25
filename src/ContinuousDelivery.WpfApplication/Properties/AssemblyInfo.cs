9b19ce16ca3f46cce18fb7baad73545c445bcfbc
Gøran Sveia Kvarv <goran.kvarv@gmail.com>
2012-02-25 23:48:48 +0100

    testing AssemblyInfo patcher in TC

--- src/ContinuousDelivery.WpfApplication/Properties/AssemblyInfo.cs
+++ src/ContinuousDelivery.WpfApplication/Properties/AssemblyInfo.cs
@@ -39,4 +39,5 @@ using System.Windows;
 	//(used if a resource is not found in the page, 
 	// app, or any theme specific resource dictionaries)
 )]
-
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
